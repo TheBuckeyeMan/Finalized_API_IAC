19e7db2609d9e85d1110905df6c1954208e530ad
Adam Maas <adamjm1220@gmail.com>
2024-10-18 16:16:46 -0400

    Lambda_Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,23 @@ resource "aws_lambda_function_url" "lambda_url" {
 }
 
 ##Add in additional code for AWS Event bridge, or API Gateway here for Lambda Deployment
+
+//GPT SAID TO TRIGGER 1 time per day
+# resource "aws_cloudwatch_event_rule" "daily_trigger" {
+#   name                = "daily-lambda-trigger"
+#   schedule_expression = "rate(1 day)"  # Runs once per day
+# }
+
+# resource "aws_cloudwatch_event_target" "lambda_target" {
+#   rule      = aws_cloudwatch_event_rule.daily_trigger.name
+#   target_id = "deleteapi-lambda"
+#   arn       = aws_lambda_function.api_lambda.arn
+# }
+
+# resource "aws_lambda_permission" "allow_eventbridge_to_invoke_lambda" {
+#   statement_id  = "AllowExecutionFromEventBridge"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.api_lambda.function_name
+#   principal     = "events.amazonaws.com"
+#   source_arn    = aws_cloudwatch_event_rule.daily_trigger.arn
+# }
